01da3daa1c593ae38bb6e705488b190746dec8e6
Turnerj <git@turnerj.com>
2020-04-03 16:03:08 +1030

    More memory efficient threading
    Also removed worker pool - it is unnecessary in this implementation

--- src/Quickenshtein/Levenshtein.Threaded.cs
+++ src/Quickenshtein/Levenshtein.Threaded.cs
@@ -11,6 +11,8 @@ namespace Quickenshtein
 {
 	public static partial class Levenshtein
 	{
+		private static WaitCallback WorkerTask = new WaitCallback(WorkerTask_CalculateSegment);
+
 		private static unsafe int CalculateDistance_MultiThreaded(ReadOnlySpan<char> source, ReadOnlySpan<char> target, CalculationOptions options)
 		{
 			var sourceLength = source.Length;
@@ -39,9 +41,7 @@ namespace Quickenshtein
 			var numberOfColumnsPerWorker = targetLength / numberOfWorkers;
 			var remainderColumns = targetLength % numberOfWorkers;
 
-			var workerRowCounter = stackalloc int[Environment.ProcessorCount];
-
-			var workerPool = ArrayPool<Task>.Shared.Rent(numberOfWorkers);
+			var rowCountPtr = stackalloc int[Environment.ProcessorCount];
 			var columnBoundariesPool = ArrayPool<int[]>.Shared.Rent(numberOfWorkers + 1);
 
 			//Initialise shared task boundaries
@@ -58,27 +58,39 @@ namespace Quickenshtein
 				Fill_Custom(startBoundaryPtr, -1, sourceLength + 1);
 			}
 
-			for (var i = 0; i < numberOfWorkers; i++)
+			for (var workerIndex = 0; workerIndex < numberOfWorkers - 1; workerIndex++)
 			{
-				var workerIndex = i;
-				workerPool[workerIndex] = Task.Run(() =>
+				var columnIndex = workerIndex * numberOfColumnsPerWorker;
+
+				ThreadPool.QueueUserWorkItem(WorkerTask, new WorkerState
 				{
-					var backColumnBoundary = columnBoundariesPool[workerIndex];
-					var forwardColumnBoundary = columnBoundariesPool[workerIndex + 1];
-					var columnIndex = workerIndex * numberOfColumnsPerWorker;
-					var targetSegmentPtr = targetPtr + columnIndex;
-					var targetSegmentLength = numberOfColumnsPerWorker;
-
-					if (workerIndex + 1 == numberOfWorkers)
-					{
-						targetSegmentLength += remainderColumns;
-					}
-
-					DoCalculateSegment(workerRowCounter, workerIndex, columnIndex, sourcePtr, sourceLength, targetSegmentPtr, targetSegmentLength, backColumnBoundary, forwardColumnBoundary);
+					RowCountPtr = rowCountPtr,
+					WorkerIndex = workerIndex,
+					ColumnIndex = columnIndex,
+					SourcePtr = sourcePtr,
+					SourceLength = sourceLength,
+					TargetSegmentPtr = targetPtr + columnIndex,
+					TargetSegmentLength = numberOfColumnsPerWorker,
+					BackColumnBoundary = columnBoundariesPool[workerIndex],
+					ForwardColumnBoundary = columnBoundariesPool[workerIndex + 1]
 				});
 			}
-			var finalWorker = workerPool[numberOfWorkers - 1];
-			finalWorker.Wait();
+
+			//Run last segment synchronously (ie. in the current thread)
+			var lastWorkerIndex = numberOfWorkers - 1;
+			var lastWorkerColumnIndex = lastWorkerIndex * numberOfColumnsPerWorker;
+			WorkerTask_CalculateSegment(new WorkerState
+			{
+				RowCountPtr = rowCountPtr,
+				WorkerIndex = numberOfWorkers - 1,
+				ColumnIndex = (numberOfWorkers - 1) * numberOfColumnsPerWorker,
+				SourcePtr = sourcePtr,
+				SourceLength = sourceLength,
+				TargetSegmentPtr = targetPtr + lastWorkerColumnIndex,
+				TargetSegmentLength = numberOfColumnsPerWorker + remainderColumns,
+				BackColumnBoundary = columnBoundariesPool[lastWorkerIndex],
+				ForwardColumnBoundary = columnBoundariesPool[lastWorkerIndex + 1]
+			});
 
 			//Extract last value in forward column boundary of last task (the actual distance)
 			var result = columnBoundariesPool[numberOfWorkers][sourceLength];
@@ -91,13 +103,22 @@ namespace Quickenshtein
 			}
 			ArrayPool<int[]>.Shared.Return(columnBoundariesPool);
 
-			ArrayPool<Task>.Shared.Return(workerPool);
-
 			return result;
 		}
 
-		private static unsafe Task DoCalculateSegment(int* workerRowCounter, int workerIndex, int columnIndex, char* sourcePtr, int sourceLength, char* targetSegmentPtr, int targetSegmentLength, int[] backColumnBoundary, int[] forwardColumnBoundary)
+		private static unsafe void WorkerTask_CalculateSegment(object state)
 		{
+			var workerState = (WorkerState)state;
+			var rowCountPtr = workerState.RowCountPtr;
+			var workerIndex = workerState.WorkerIndex;
+			var columnIndex = workerState.ColumnIndex;
+			var sourcePtr = workerState.SourcePtr;
+			var sourceLength = workerState.SourceLength;
+			var targetSegmentPtr = workerState.TargetSegmentPtr;
+			var targetSegmentLength = workerState.TargetSegmentLength;
+			var backColumnBoundary = workerState.BackColumnBoundary;
+			var forwardColumnBoundary = workerState.ForwardColumnBoundary;
+
 			var arrayPool = ArrayPool<int>.Shared;
 			var pooledArray = arrayPool.Rent(targetSegmentLength);
 
@@ -106,17 +127,14 @@ namespace Quickenshtein
 				//TODO: Support intrinsics for FillRow_Custom
 				Fill_Custom(previousRowPtr, columnIndex, targetSegmentLength);
 
-				ref var selfWorkerRowCount = ref workerRowCounter[workerIndex];
+				ref var selfWorkerRowCount = ref rowCountPtr[workerIndex];
 
 				for (var rowIndex = 0; rowIndex < sourceLength;)
 				{
 					if (workerIndex > 0)
 					{
-						ref var previousWorkerRowCount = ref workerRowCounter[workerIndex - 1];
-						while (Interlocked.CompareExchange(ref previousWorkerRowCount, 0, 0) == rowIndex)
-						{
-							//No-op :(
-						}
+						ref var previousWorkerRowCount = ref rowCountPtr[workerIndex - 1];
+						while (Interlocked.CompareExchange(ref previousWorkerRowCount, 0, 0) == rowIndex);
 					}
 
 					var lastSubstitutionCost = backColumnBoundary[rowIndex];
@@ -142,7 +160,6 @@ namespace Quickenshtein
 				}
 
 				arrayPool.Return(pooledArray);
-				return Task.CompletedTask;
 			}
 		}
 
---
+++ src/Quickenshtein/WorkerState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quickenshtein
+{
+	internal unsafe class WorkerState
+	{
+		public int* RowCountPtr;
+		public int WorkerIndex;
+
+		public int ColumnIndex;
+
+		public char* SourcePtr;
+		public int SourceLength;
+		public char* TargetSegmentPtr;
+		public int TargetSegmentLength;
+
+		public int[] BackColumnBoundary;
+		public int[] ForwardColumnBoundary;
+	}
+}
