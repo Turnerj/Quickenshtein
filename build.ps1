770776b6ac0939f80edee8b754cbae852cf98e82
Turnerj <git@turnerj.com>
2020-04-05 01:46:21 +1030

    Added test environment messages to non-coverage tests

--- build.ps1
+++ build.ps1
@@ -47,6 +47,7 @@ if ($RunTests) {
 		$env:COMPlus_EnableAVX2 = 1
 		$env:COMPlus_EnableSSE41 = 1
 		$env:COMPlus_EnableSSE2 = 1
+		Write-Host "Test Environment: Normal" -ForegroundColor "Cyan"
 		dotnet test $config.TestProject
 		if ($LastExitCode -ne 0) {
 			Write-Host "Tests failed, aborting build!" -Foreground "Red"
@@ -56,6 +57,7 @@ if ($RunTests) {
 		$env:COMPlus_EnableAVX2 = 0
 		$env:COMPlus_EnableSSE41 = 1
 		$env:COMPlus_EnableSSE2 = 1
+		Write-Host "Test Environment: AVX2 Disabled" -ForegroundColor "Cyan"
 		dotnet test $config.TestProject --framework netcoreapp3.1
 		if ($LastExitCode -ne 0) {
 			Write-Host "Tests failed, aborting build!" -Foreground "Red"
@@ -65,6 +67,7 @@ if ($RunTests) {
 		$env:COMPlus_EnableAVX2 = 0
 		$env:COMPlus_EnableSSE41 = 0
 		$env:COMPlus_EnableSSE2 = 1
+		Write-Host "Test Environment: SSE41 Disabled" -ForegroundColor "Cyan"
 		dotnet test $config.TestProject --framework netcoreapp3.1
 		if ($LastExitCode -ne 0) {
 			Write-Host "Tests failed, aborting build!" -Foreground "Red"
@@ -74,6 +77,7 @@ if ($RunTests) {
 		$env:COMPlus_EnableAVX2 = 0
 		$env:COMPlus_EnableSSE41 = 0
 		$env:COMPlus_EnableSSE2 = 0
+		Write-Host "Test Environment: SSE2 Disabled" -ForegroundColor "Cyan"
 		dotnet test $config.TestProject --framework netcoreapp3.1
 		if ($LastExitCode -ne 0) {
 			Write-Host "Tests failed, aborting build!" -Foreground "Red"
