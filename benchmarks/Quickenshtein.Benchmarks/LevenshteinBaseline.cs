c4e124f86023654d91de52d1975c2b883073acea
Turnerj <git@turnerj.com>
2020-10-20 22:32:38 +1030

    Minor tweaks to baseline variant

--- benchmarks/Quickenshtein.Benchmarks/LevenshteinBaseline.cs
+++ benchmarks/Quickenshtein.Benchmarks/LevenshteinBaseline.cs
@@ -10,11 +10,9 @@ namespace Quickenshtein.Benchmarks
 	{
 		public static int GetDistance(string source, string target)
 		{
-			var targetLength = target.Length;
-
 			var costMatrix = Enumerable
 			  .Range(0, source.Length + 1)
-			  .Select(line => new int[targetLength + 1])
+			  .Select(line => new int[target.Length + 1])
 			  .ToArray();
 
 			for (var i = 1; i <= source.Length; ++i)
@@ -31,11 +29,11 @@ namespace Quickenshtein.Benchmarks
 			{
 				for (var j = 1; j <= target.Length; ++j)
 				{
-					var insert = costMatrix[i][j - 1] + 1;
-					var delete = costMatrix[i - 1][j] + 1;
-					var edit = costMatrix[i - 1][j - 1] + (source[i - 1] == target[j - 1] ? 0 : 1);
+					var insertion = costMatrix[i][j - 1] + 1;
+					var deletion = costMatrix[i - 1][j] + 1;
+					var substitution = costMatrix[i - 1][j - 1] + (source[i - 1] == target[j - 1] ? 0 : 1);
 
-					costMatrix[i][j] = Math.Min(Math.Min(insert, delete), edit);
+					costMatrix[i][j] = Math.Min(Math.Min(insertion, deletion), substitution);
 				}
 			}
 
